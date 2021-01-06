b0681dbf98d439c9de65a6ba7a5966e16026ea9d
Vitor Freitas <vitor_freitas_b@hotmail.com>
2021-01-06 00:58:51 -0300

    feat(world): add world grasses

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var GrassEffect = load("res://Effects/GrassEffect.tscn")
-		var grass_effect = GrassEffect.instance()
-		var world = get_tree().current_scene
-		world.add_child(grass_effect)
-		grass_effect.global_position = global_position
-		queue_free()
+func create_grass_effect():
+	var GrassEffect = load("res://Effects/GrassEffect.tscn")
+	var grass_effect = GrassEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(grass_effect)
+	grass_effect.global_position = global_position
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
