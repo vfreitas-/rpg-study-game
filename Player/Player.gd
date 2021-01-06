9f4b46571d4c978d7471033059e664745d40c5d2
Vitor Freitas <vitor_freitas_b@hotmail.com>
2021-01-06 01:50:18 -0300

    feat(player): add roll state

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
+const ROLL_SPEED = 110
 
 enum State {
 	ATTACK,
@@ -12,6 +13,7 @@ enum State {
 
 var state = State.MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.DOWN
 
 onready var animationTree = $AnimationTree
 # pega os valores do State Machine da animation tree
@@ -26,7 +28,7 @@ func _ready():
 # _physics_process: quando acessamos alguma propriedade que tenha a ver com o posicionamento
 # do KitematicBody por exemplo, com a fisica dele
 # _process: quando não acessamos esses valores e temos uma função mais pura com valores próprios
-func _process(delta):
+func _physics_process(delta):
 	match state:
 		State.ATTACK:
 			handle_attack(delta)
@@ -45,10 +47,12 @@ func handle_move(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		# seta os valores para Idle e Run da animation tree
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		# define com o state de run
 		animationState.travel("Run")
 		# Se move na até o valor X, baseado no valor Y
@@ -57,7 +61,10 @@ func handle_move(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = State.ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = State.ATTACK
@@ -66,8 +73,18 @@ func handle_attack(_delta):
 	velocity = Vector2.ZERO
 	animationState.travel("Attack")
 	
-func handle_roll(delta):
-	print("Roll!")
+func handle_roll(_delta):
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
+	
+# Callbacks and Signals
+func handle_roll_animation_end():
+	velocity = velocity * 0.8
+	state = State.MOVE
 	
 func handle_attack_animation_end():
 	state = State.MOVE
